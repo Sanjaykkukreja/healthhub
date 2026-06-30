d0edeee9c45072da3560fc694920584a533a6ba5
Sanjaykkukreja <33394448+Sanjaykkukreja@users.noreply.github.com>
2026-06-30 23:22:05 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 //  Get these from: supabase.com → your project → Settings → API
 // ════════════════════════════════════════════════════
 
-const SUPABASE_URL = "https://oshuhhrvdmnsqvuavndu.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://oshuhhrvdmnsqvuavndu.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zaHVoaHJ2ZG1uc3F2dWF2bmR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MzkwNzEsImV4cCI6MjA5ODExNTA3MX0.6xTkWRMVI1bJiEJquYpgsDfcXp47hU7W2kH7hwdtLP4";
 
 // Do not edit below this line
